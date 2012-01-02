6e18abe8f0f5f3579269b6aa77db12249b5cd083
Dario Seminara <darios3@gmail.com>
2012-01-02 15:11:32 -0300

    completed implementation of main executable to_erb

--- lib/to_erb.rb
+++ lib/to_erb.rb
@@ -1,5 +1,18 @@
 require "to_erb/version"
+require "to_erb/erb_engine"
 
 module ToErb
-  # Your code goes here...
+  def self.convert(infile,outfile)
+
+
+    hamlcode = File.open(infile).read
+    engine = ErbEngine.new(hamlcode)
+    File.open(outfile,"w") do |file|
+      file.write engine.render
+    end
+
+    print "converted #{infile} to #{outfile}\n"
+  rescue Exception => e
+    print "FAILED conersion of #{infile} due error #{e}"
+  end
 end
